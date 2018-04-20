c38f8d3ebab3e886c2e52e7cf63136aa96ff6d4a
Justin Menkveld <justin@menklab.com>
2018-04-20 12:02:46 -0400

    working towards automated deployment

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-# Nurture Video Feeds
+# AWS Administrator Functions
+
+## Install
+`npm install`
+
+## Deploy
+`./sls.sh admin deploy --aws-profile <PROFILE> --account-id <####>`
+
+
+## How It Works
+##### Backup EC2 Volumes
+Snapshot volume.And rotate old snapshots. The tags must be applied to the volume. But if you add it to the ec2 instance using the tag copy function below it will end up on the volume as well.
+
+- tag name: `snapshot_rotation`
+- tag value: `int for days to keep`
+
+##### Tag EC2 Volumes
+This applies any tag found on the ec2 instance to the corresponding and attached volumes
+
+##### Update CNAME On Boot
+This will update a cname record in route53 with the new ipadress of the ec2 instance at boot
+- tag name: `cname`
+- tag value `sub.domain.com`
+- tag name: `zoneId`
+- tag value `<ZONE ID>`
+
+
+
 
 ### Helpful Tools
 - cwtail - used to tail lambda functions  
--- sls.sh
+++ sls.sh
@@ -11,10 +11,7 @@ function show_help {
 A helper script to run serverless service stacks from their location
 sls <serverless service> [options]
 options:
- authentication
- domain
- repositories
- api
+ admin
 EOF
     exit 0
 }
@@ -45,40 +42,12 @@ fi
 case "${ACTION}" in
 
     # only used for first deploy
-    authentication)
-        pushd infrastructure/Authentication
-        runServerless
-        popd
-        ;;
 
-    domain)
-        pushd Domain
+    admin)
+#        pushd ./
         runServerless
-        popd
+#        popd
         ;;
-
-    repositories)
-        pushd Repositories
-        runServerless
-        popd
-        ;;
-
-    permissions)
-        pushd infrastructure/Permissions
-        runServerless
-        popd
-        ;;
-
-    # 1. create appsync first with ./sls api deploy-appsync ...
-    # 2. update appsync after with ./sls api update-appsync ...
-    api)
-        pushd API
-        eval "${PROJECT_ROOT}/node_modules/.bin/gql-merge -o schema.graphql GraphQL/**/*.graphqls"
-        eval "${PROJECT_ROOT}/node_modules/.bin/gqlschema schema.graphql -i -o graphql.schema.json"
-        runServerless
-        popd
-        ;;
-
     *)
         show_help
         exit 0
